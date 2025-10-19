f68db0d0f0f0dd9a8e5e311cc557fabc8f3db320
hsgamer <huynhqtienvtag@gmail.com>
2025-10-19 22:08:36 +0700

    support nested editor on EditorStatusDisplay

--- editor-extra/src/main/java/io/github/projectunified/minigamecore/editor/extra/status/EditorStatusDisplay.java
+++ editor-extra/src/main/java/io/github/projectunified/minigamecore/editor/extra/status/EditorStatusDisplay.java
@@ -92,39 +92,40 @@ public abstract class EditorStatusDisplay<B, S, T> {
     public T display(Editor<?> editor) {
         B builder = newBuilder();
         Deque<Entry> deque = new ArrayDeque<>();
-        deque.addFirst(new Entry(0, "ROOT", editor, false));
+        deque.addFirst(new Entry(0, "ROOT", editor, editor, false));
         while (!deque.isEmpty()) {
             Entry entry = deque.removeFirst();
-            int level = entry.level();
-            Object value = entry.value();
-            Object preprocessedValue = preprocessValue(value, editor);
+            int level = entry.level;
+            Object value = entry.value;
+            Object preprocessedValue = preprocessValue(value, entry.editor);
             if (preprocessedValue != value) {
-                deque.addFirst(new Entry(level, entry.key(), preprocessedValue, entry.fromCollection()));
+                deque.addFirst(new Entry(level, entry.key, preprocessedValue, entry.editor, entry.fromCollection));
                 continue;
             }
 
             if (value instanceof Editor<?>) {
-                deque.addFirst(new Entry(level, entry.key(), ((Editor<?>) value).status(), false));
+                Editor<?> subEditor = (Editor<?>) value;
+                deque.addFirst(new Entry(level, entry.key, subEditor.status(), subEditor, false));
                 continue;
             }
 
             S section = newSection(builder, level);
 
-            appendKey(section, entry.key(), entry.fromCollection());
+            appendKey(section, entry.key, entry.fromCollection);
             if (value instanceof Collection<?>) {
                 Collection<?> collection = (Collection<?>) value;
                 appendSize(section, collection.size());
                 List<Object> copyList = new ArrayList<>(collection);
                 for (int i = copyList.size() - 1; i >= 0; i--) {
                     Object o = copyList.get(i);
-                    deque.addFirst(new Entry(level + 1, Integer.toString(i), o, true));
+                    deque.addFirst(new Entry(level + 1, Integer.toString(i), o, entry.editor, true));
                 }
             } else if (value.getClass().isArray()) {
                 int length = Array.getLength(value);
                 appendSize(section, length);
                 for (int i = length - 1; i >= 0; i--) {
                     Object o = Array.get(value, i);
-                    deque.addFirst(new Entry(level + 1, Integer.toString(i), o, true));
+                    deque.addFirst(new Entry(level + 1, Integer.toString(i), o, entry.editor, true));
                 }
             } else if (value instanceof Map<?, ?>) {
                 Map<?, ?> subMap = (Map<?, ?>) value;
@@ -132,13 +133,13 @@ public abstract class EditorStatusDisplay<B, S, T> {
                 List<Map.Entry<?, ?>> copyList = new ArrayList<>(subMap.entrySet());
                 for (int i = subMap.size() - 1; i >= 0; i--) {
                     Map.Entry<?, ?> o = copyList.get(i);
-                    deque.addFirst(new Entry(level + 1, Objects.toString(o.getKey()), o.getValue(), false));
+                    deque.addFirst(new Entry(level + 1, Objects.toString(o.getKey()), o.getValue(), entry.editor, false));
                 }
             } else if (value instanceof Map.Entry<?, ?>) {
                 Map.Entry<?, ?> subEntry = (Map.Entry<?, ?>) value;
-                deque.addFirst(new Entry(level + 1, Objects.toString(subEntry.getKey()), subEntry.getValue(), false));
+                deque.addFirst(new Entry(level + 1, Objects.toString(subEntry.getKey()), subEntry.getValue(), entry.editor, false));
             } else {
-                appendValue(section, value, editor);
+                appendValue(section, value, entry.editor);
             }
             builder = addToBuilder(section, builder);
         }
@@ -149,45 +150,15 @@ public abstract class EditorStatusDisplay<B, S, T> {
         private final int level;
         private final String key;
         private final Object value;
+        private final Editor<?> editor;
         private final boolean fromCollection;
 
-        private Entry(int level, String key, Object value, boolean fromCollection) {
+        private Entry(int level, String key, Object value, Editor<?> editor, boolean fromCollection) {
             this.level = level;
             this.key = key;
             this.value = value;
+            this.editor = editor;
             this.fromCollection = fromCollection;
-        }
-
-        public int level() {
-            return level;
-        }
-
-        public String key() {
-            return key;
-        }
-
-        public Object value() {
-            return value;
-        }
-
-        public boolean fromCollection() {
-            return fromCollection;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this) return true;
-            if (obj == null || obj.getClass() != this.getClass()) return false;
-            Entry that = (Entry) obj;
-            return this.level == that.level &&
-                    Objects.equals(this.key, that.key) &&
-                    Objects.equals(this.value, that.value) &&
-                    this.fromCollection == that.fromCollection;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(level, key, value, fromCollection);
         }
     }
 }
