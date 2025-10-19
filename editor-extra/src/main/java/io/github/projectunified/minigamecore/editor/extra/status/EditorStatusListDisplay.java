e54c906ca121cafb6628bc1cc6bcce285726b125
hsgamer <huynhqtienvtag@gmail.com>
2025-10-19 23:48:23 +0700

    comment to EditorStatusListDisplay

--- editor-extra/src/main/java/io/github/projectunified/minigamecore/editor/extra/status/EditorStatusListDisplay.java
+++ editor-extra/src/main/java/io/github/projectunified/minigamecore/editor/extra/status/EditorStatusListDisplay.java
@@ -3,6 +3,11 @@ package io.github.projectunified.minigamecore.editor.extra.status;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * An {@link EditorStatusDisplay} that converts the status of an {@link io.github.projectunified.minigamecore.editor.Editor} to a list
+ *
+ * @param <S> the type of the element in the list
+ */
 public abstract class EditorStatusListDisplay<S> extends EditorStatusDisplay<List<S>, S, List<S>> {
     @Override
     protected List<S> newBuilder() {
